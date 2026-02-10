a96025fcec5c44661298ed4d89decdf77e6c65db
Shaimal <shaimalabdulla35@gmail.com>
2026-02-10 18:24:26 +0500

    Add routing for multi page support

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
+import { Routes, Route } from 'react-router'
 import HomePage from './Pages/HomePage'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <HomePage />
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/checkout" element={<div>Test</div>} />
+      </Routes>
   )
 }
 
